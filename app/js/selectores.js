276a47871d78acb8e0a8ed79a6ac5b3502083b3b
Jose Maria <telecomu4@gmail.com>
2017-02-27 18:36:56 +0100

    creado archivo js del ejemplo

---
+++ app/js/selectores.js
@@ -0,0 +1,4 @@
+document.getElementById("js_container1").innerHTML = "salida: con getElementById";
+js_container2.innerHTML = "salida: A tavés del objeto que se crea de un identificador";
+document.querySelector("#js_container3").innerHTML = "salida: con querySelector"; 
+document.querySelector(".container").innerHTML = "salida: con querySelector, pero a través de la clase";
